f4e9005e8387c24f0aab0ed230379ff0036934ec
Miguel Angel Serna Montoya <migueloserna@gmail.com>
2024-02-16 10:38:04 -0500

    comfy

---
+++ src/cart/toggleCart.js
@@ -0,0 +1,14 @@
+import { getElement } from '../utils.js';
+const cartOverlay = getElement('.cart-overlay');
+const closeBtn = getElement('.cart-close');
+const toggleCartBtn = getElement('.toggle-cart');
+
+toggleCartBtn.addEventListener('click', () => {
+  cartOverlay.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  cartOverlay.classList.remove('show');
+});
+export const openCart = () => {
+  cartOverlay.classList.add('show');
+};
